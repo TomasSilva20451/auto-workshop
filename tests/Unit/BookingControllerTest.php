0c6ef9d09e3c774414f4843f0dc6b07763e24bae
TomasSilva20451 <a20451@alunos.ipca.pt>
2023-05-19 12:40:24 +0100

    remove bookingtest

--- tests/Unit/BookingControllerTest.php
+++ tests/Unit/BookingControllerTest.php
@@ -1,43 +1,163 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace Tests\Unit\Controllers;
 
+use App\Http\Controllers\BookingController;
 use App\Models\Booking;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
+use Mockery;
+use Tests\TestCase;
 
-class BookingController extends Controller
+class BookingControllerTest extends TestCase
 {
-    public function index()
+    public function testIndex()
     {
-        $bookings = Booking::all();
-        return response()->json($bookings);
+        $mockedBookings = [
+            Booking::factory()->create(),
+            Booking::factory()->create(),
+        ];
+
+        $bookingMock = Mockery::mock(Booking::class);
+        $bookingMock->shouldReceive('all')->andReturn($mockedBookings);
+
+        $controller = new BookingController($bookingMock);
+        $response = $controller->index();
+
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertCount(2, json_decode($response->getContent()));
     }
 
-    public function store(Request $request)
+    public function testStore()
     {
-        // Logic for creating a new booking goes here
+        $request = new Request([
+            'BookingID' => 1,
+            'VehicleID' => 1,
+            'ClientID' => 1,
+            'BookingDate' => '2023-05-19',
+            'BookingType' => 'Type A',
+            'Status' => 'Active',
+            'CreationDate' => '2023-05-19',
+            'CustomerID' => 1,
+            'created_at' => '2023-05-19',
+            'updated_at' => '2023-05-19',
+            'appointment_id' => 1,
+            'vehicle_id' => 1,
+            'client_id' => 1,
+            'booking_date' => '2023-05-19',
+            'booking_type' => 'Type A',
+        ]);
 
-        return response()->json(['message' => 'Booking created successfully'], 201);
+        $bookingMock = Mockery::mock(Booking::class);
+        $bookingMock->shouldReceive('create')->with($request->all())->andReturn($bookingMock);
+
+        $controller = new BookingController($bookingMock);
+        $response = $controller->store($request);
+
+        $this->assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
+        $this->assertEquals('Booking created successfully', json_decode($response->getContent())->message);
     }
 
-    public function show($id)
+    public function testStoreWithInvalidData()
     {
-        $booking = Booking::find($id);
-        return response()->json($booking);
+        $this->expectException(ValidationException::class);
+
+        $request = new Request();
+
+        $controller = new BookingController();
+        $controller->store($request);
     }
 
-    public function update(Request $request, $id)
+    public function testShow()
     {
-        // Logic for updating a booking goes here
+        $bookingMock = Mockery::mock(Booking::class);
+        $bookingMock->shouldReceive('find')->with(1)->andReturn($bookingMock);
 
-        return response()->json(['message' => 'Booking updated successfully']);
+        $controller = new BookingController($bookingMock);
+        $response = $controller->show(1);
+
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertEquals($bookingMock, json_decode($response->getContent()));
     }
 
-    public function destroy($id)
+    public function testShowWithNonExistingBooking()
     {
-        $booking = Booking::find($id);
-        $booking->delete();
+        $bookingMock = Mockery::mock(Booking::class);
+        $bookingMock->shouldReceive('find')->with(1)->andReturn(null);
 
-        return response()->json(['message' => 'Booking deleted successfully']);
+        $controller = new BookingController($bookingMock);
+        $response = $controller->show(1);
+
+        $this->assertEquals(Response::HTTP_NOT_FOUND, $response->getStatusCode());
+        $this->assertEquals('Booking not found', json_decode($response->getContent())->message);
+    }
+
+    public function testUpdate()
+    {
+        $request = new Request([
+            'BookingID' => 1,
+            'VehicleID' => 1,
+            'ClientID' => 1,
+            'BookingDate' => '2023-05-19',
+            'BookingType' => 'Type A',
+            'Status' => 'Active',
+            'CreationDate' => '2023-05-19',
+            'CustomerID' => 1,
+            'created_at' => '2023-05-19',
+            'updated_at' => '2023-05-19',
+            'appointment_id' => 1,
+            'vehicle_id' => 1,
+            'client_id' => 1,
+            'booking_date' => '2023-05-19',
+            'booking_type' => 'Type A',
+        ]);
+
+        $bookingMock = Mockery::mock(Booking::class);
+        $bookingMock->shouldReceive('find')->with(1)->andReturn($bookingMock);
+        $bookingMock->shouldReceive('save')->once();
+
+        $controller = new BookingController($bookingMock);
+        $response = $controller->update($request, 1);
+
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertEquals('Booking updated successfully', json_decode($response->getContent())->message);
+    }
+
+    public function testUpdateWithNonExistingBooking()
+    {
+        $bookingMock = Mockery::mock(Booking::class);
+        $bookingMock->shouldReceive('find')->with(1)->andReturn(null);
+
+        $controller = new BookingController($bookingMock);
+        $response = $controller->update(new Request(), 1);
+
+        $this->assertEquals(Response::HTTP_NOT_FOUND, $response->getStatusCode());
+        $this->assertEquals('Booking not found', json_decode($response->getContent())->message);
+    }
+
+    public function testDestroy()
+    {
+        $bookingMock = Mockery::mock(Booking::class);
+        $bookingMock->shouldReceive('find')->with(1)->andReturn($bookingMock);
+        $bookingMock->shouldReceive('delete')->once();
+
+        $controller = new BookingController($bookingMock);
+        $response = $controller->destroy(1);
+
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertEquals('Booking deleted successfully', json_decode($response->getContent())->message);
+    }
+
+    public function testDestroyWithNonExistingBooking()
+    {
+        $bookingMock = Mockery::mock(Booking::class);
+        $bookingMock->shouldReceive('find')->with(1)->andReturn(null);
+
+        $controller = new BookingController($bookingMock);
+        $response = $controller->destroy(1);
+
+        $this->assertEquals(Response::HTTP_NOT_FOUND, $response->getStatusCode());
+        $this->assertEquals('Booking not found', json_decode($response->getContent())->message);
     }
 }
