e57fc4659462ffeacee5df5397fc405d05f77e49
TomasSilva20451 <a20451@alunos.ipca.pt>
2023-05-19 16:08:38 +0100

    dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,45 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\View;
-use App\Models\Sale;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\Vehicle;
+use App\Models\Sale;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
-    // DashboardController.php
+    public function index()
+    {
+        return view('dashboard.index');
+    }
 
     public function realTimeSalesData()
     {
-        // Logic for retrieving real-time sales data goes here
         $totalSales = Sale::count();
         $totalRevenue = Sale::sum('price');
         $averageSellingPrice = $totalSales > 0 ? $totalRevenue / $totalSales : 0;
 
-        // Pass the data to the view
         $salesData = [
             'sales' => $totalSales,
             'revenue' => $totalRevenue,
             'average_selling_price' => $averageSellingPrice,
         ];
 
-        // Return the view with the data
         return view('dashboard.real-time-sales-data', $salesData);
     }
 
-
     public function generateVisualizations()
     {
-        // Logic for generating visualizations goes here
         $vehiclesByMake = Vehicle::select('make', DB::raw('COUNT(*) as count'))
             ->groupBy('make')
             ->get();
 
         $visualization1 = $vehiclesByMake->pluck('count')->toArray();
         $visualization2 = $vehiclesByMake->pluck('make')->toArray();
-        // Generate other visualizations as per your requirement
 
         return response()->json([
             'visualization_1' => $visualization1,
@@ -51,7 +47,6 @@ class DashboardController extends Controller
 
     public function fetchHistoricalData()
     {
-        // Logic for fetching historical data goes here
         $salesData = Sale::select('date', DB::raw('COUNT(*) as count'), DB::raw('SUM(price) as total_revenue'))
             ->groupBy('date')
             ->get();
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\BookingController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\BookingController;
 use App\Http\Controllers\VehicleController;
 use App\Http\Controllers\PartController;
 use App\Http\Controllers\ServiceHistoryController;
@@ -20,27 +20,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/appointments', [AppointmentController::class, 'store'])->name('appointments.store');
 
 // Rest of the routes
 
+// appointments
+Route::post('/appointments', [AppointmentController::class, 'store'])->name('appointments.store');
+
+// bookings
 Route::post('/bookings', [BookingController::class, 'store']);
 Route::get('/bookings', [BookingController::class, 'index']);
 Route::get('/bookings/{id}', [BookingController::class, 'show']);
 Route::put('/bookings/{id}', [BookingController::class, 'update']);
 
+// service-history
 Route::post('/service-history', [ServiceHistoryController::class, 'store'])->name('service-history.store');
 Route::get('/service-history/{vehicle_id}', [ServiceHistoryController::class, 'show'])->name('service-history.show');
 
+// parts
 Route::post('/parts', [PartController::class, 'store'])->name('parts.store');
 Route::get('/parts', [PartController::class, 'index'])->name('parts.index');
 
+// vehicles
 Route::get('/vehicles', [VehicleController::class, 'index'])->name('vehicles.index');
 Route::post('/vehicles', [VehicleController::class, 'store'])->name('vehicles.store');
 
-Route::get('/real-time-sales-data', [DashboardController::class, 'getRealTimeSalesData'])->name('real-time-sales-data');
+// dashboard 
+Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/dashboard/real-time-sales', [DashboardController::class, 'realTimeSalesData']);
+Route::get('/dashboard/visualizations', [DashboardController::class, 'generateVisualizations']);
+Route::get('/dashboard/historical-data', [DashboardController::class, 'fetchHistoricalData']);
 
-Route::get('/send-email', [AppointmentController::class, 'sendEmail'])->name('send.email');
+// real-time-sales-data
+Route::get('/real-time-sales-data', [DashboardController::class, 'getRealTimeSalesData'])->name('real-time-sales-data');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
