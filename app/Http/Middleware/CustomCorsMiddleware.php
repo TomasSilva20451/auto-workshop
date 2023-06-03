93cc26590400805969245ca3dde57ec09caa11ad
TomasSilva20451 <a20451@alunos.ipca.pt>
2023-06-03 01:06:19 +0100

    backend 1

--- app/Http/Middleware/CustomCorsMiddleware.php
+++ app/Http/Middleware/CustomCorsMiddleware.php
@@ -10,9 +10,18 @@ class CustomCorsMiddleware
     {
         $response = $next($request);
 
-        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:3000');
+        $allowedOrigins = [
+            'http://localhost:5173',
+            'http://localhost:3000',
+        ];
+
+        if (in_array($request->header('Origin'), $allowedOrigins)) {
+            $response->headers->set('Access-Control-Allow-Origin', $request->header('Origin'));
+            $response->headers->set('Access-Control-Allow-Credentials', 'true');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-XSRF-TOKEN, x-xsrf-token');
+        }
+
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
         return $response;
     }
